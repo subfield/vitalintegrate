1e8eb022b7abd4629f1e10f849fd291f7a083190
subfield <tetelainv@gmail.com>
2024-03-25 13:45:49 +0100

    debug (002)

--- src/api/cron.js
+++ src/api/cron.js
@@ -4,7 +4,7 @@ import { Axios } from "../config/libs";
 export default async function handler(req, res) {
   try {
     await Axios.get(`${SERVER}`);
-    res.status(200).end('ping');
+    res.status(200).end('ping...');
   } catch (error) {
     res.status(500).end('Internal Server Error.');
   }
